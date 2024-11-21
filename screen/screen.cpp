2b2d8caf674f68b30c46302b224fbb6d04393d76
morena1001 <josueflores15.jf@gmail.com>
2024-11-20 23:34:57 -0600

    Trying to make bitmap for screen work

--- screen/screen.cpp
+++ screen/screen.cpp
@@ -1,25 +1,70 @@
 #include <iostream>
 #include <SDL2/SDL.h>
 
+#include "screen.h"
+
 using namespace std;
 
-const int WIDTH = 800, HEIGHT = 600;
-
 int main (int argc, char** argv) {
-    SDL_Init (SDL_INIT_EVERYTHING );
-    SDL_Window *window = SDL_CreateWindow ("1001_x8", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
+    SDL_Init (SDL_INIT_EVERYTHING);
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    
+    SDL_CreateWindowAndRenderer (WIDTH, HEIGHT, 0, &window, &renderer);
 
-    if (window == NULL) {
-        cout << "Could not create window: " << SDL_GetError () << endl;
-        return 1;
+    SDL_SetRenderDrawColor (renderer, 50, 48, 47, 255);
+    SDL_RenderClear (renderer);
+    SDL_SetRenderDrawColor (renderer, 255, 255, 255, 255);
+
+    uint8_t char_pos_y = 1, char_pos_x = 1;
+
+    for (int i = 0; i < 69; i++) {
+        for (int j = 0; j < 7; j++) {
+            for (int k = 0; k < 5; k++) {
+                if ((characters[i][j] >> k) & 1) 
+                    SDL_RenderDrawPoint (renderer, k + char_pos_x, j + char_pos_y);
+            }
+        }
+        char_pos_x += 7;
+        SDL_RenderPresent (renderer);
     }
+    
+    SDL_RenderPresent (renderer);
+
+    // for (int i = 0; i < 7; i++) {
+    //     for (int j = 0; j < 5; j++) {
+    //         if ((characters[char_A_idx][i] >> j) & 1)
+    //             SDL_RenderDrawPoint (renderer, j + char_pos_x, i + char_pos_y);
+    //     }
+    // }
+    // char_pos_x += 6;
+    // SDL_RenderPresent (renderer);
+
+    // for (int i = 0; i < 7; i++) {
+    //     for (int j = 0; j < 5; j++) {
+    //         if ((characters[char_B_idx][i] >> j) & 1)
+    //             SDL_RenderDrawPoint (renderer, j + char_pos_x, i + char_pos_y);
+    //     }
+    // }
+    // char_pos_x +=6;
+    // SDL_RenderPresent (renderer);
+
+    // for (int i = 0; i < 7; i++) {
+    //     for (int j = 0; j < 5; j++) {
+    //         if ((characters[char_C_idx][i] >> j) & 1)
+    //             SDL_RenderDrawPoint (renderer, j + char_pos_x, i + char_pos_y);
+    //     }
+    // }
+    // char_pos_x +=6;
+    // SDL_RenderPresent (renderer);
+    
+
 
     SDL_Event windowEvent;
-
     while (1) {
         if (SDL_PollEvent (&windowEvent)) {
             if (windowEvent.type == SDL_QUIT)       break;
-
+                
 
         }
     }
@@ -27,7 +72,6 @@ int main (int argc, char** argv) {
     SDL_DestroyWindow (window);
     SDL_Quit ();
 
-    return 0;
 }
 
 
