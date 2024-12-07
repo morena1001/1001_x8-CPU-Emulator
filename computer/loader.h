b3e1b2715404416c39214d7913a9f8b281c40fb7
josue-flores03 <josue.flores03@utrgv.edu>
2024-12-06 23:40:14 -0600

    Update to subroutines!

--- computer/loader.h
+++ computer/loader.h
@@ -12,6 +12,7 @@ using namespace std;
 #define IS_HALT_OPCODE(opcode)          (opcode == 0x35)
 #define IS_LABEL_ENCODING(opcode)       (opcode == 0x36)
 #define IS_START_ENCODING(opcode)       (opcode == 0x37)
+#define IS_SUBR_ENCODING(opcode)        (opcode == 0x41)
 
 #define IS_NUMBER(value)                (value == '0' || value == '1' || value == '2' || value == '3' || value == '4' || value == '5' || value == '6' || value == '7' || value == '8' || value == '9')
 #define INT_REPRESENTATION_OF_HEX(hex_value)    (IS_NUMBER (hex_value) ? ((u32) hex_value) - 48 : (hex_value == 'A' ? 10 : (hex_value == 'B' ? 11 : (hex_value == 'C' ? 12 : (hex_value == 'D' ? 13 : (hex_value == 'E' ? 14 : (hex_value == 'F' ? 15 : 0)))))))
@@ -20,7 +21,8 @@ using namespace std;
 
 #define REG_AS_OPERAND1(opcode)         ((opcode >= 0x01 && opcode <= 0x03) || opcode == 0x05 || opcode == 0x07 || opcode == 0x08 || (opcode >= 0x0E && opcode <= 0x19) || opcode == 0x1B || opcode == 0x1D || opcode == 0x1E || opcode == 0x38 || opcode == 0x3C)
 #define MEM_AS_OPERAND1(opcode)         (opcode == 0x04 || opcode == 0x06 || opcode == 0x09 || opcode == 0x1A || opcode == 0x1C || opcode == 0x1F || opcode == 0x20 || (opcode >= 0x39 && opcode <= 0x3B) || (opcode >= 0x3D && opcode <= 0x40))
-#define LAB_AS_OPERAND1(opcode)         (opcode == 0x21 || opcode == 0x22 || (opcode >= 0x24 && opcode <= 0x2B))
+#define LAB_AS_OPERAND1(opcode)         (opcode == 0x21 || (opcode >= 0x24 && opcode <= 0x2B))
+#define SBR_AS_OPERAND1(opcode)         (opcode == 0x22)
 
 #define REG_AS_OPERAND2(opcode)         (opcode == 0x03 || opcode == 0x04 || opcode == 0x05 || opcode == 0x0E || opcode == 0x0F || opcode == 0x10 || (opcode >= 0x12 && opcode <= 0x16) || opcode == 0x39)
 #define MEM_AS_OPERAND2(opcode)         (opcode == 0x01 || opcode == 0x06 || opcode == 0x17 || opcode == 0x38 || opcode == 0x3A || opcode == 0x3C)
