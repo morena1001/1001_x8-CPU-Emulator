336028fcd478ef0487193339f60c72eed4a88b37
josue-flores03 <josue.flores03@utrgv.edu>
2024-12-31 16:50:04 -0600

    Finished up to label encoding check in 2nd loop. Tested up to halt instruction check in 2nd loop.

--- computer/loader/loader.cpp
+++ computer/loader/loader.cpp
@@ -29,7 +29,7 @@ void Load_Program (string file_path, cpu_t& cpu, mem_t& mem) {
 
     // ADDRESSES FOR LOADER PROGRAM
     word address = 0xE086;
-    word var_address = 0x0FF64;
+    word var_address = 0x0FF57;
 
     // ADDRESSES FOR OS PROGRAM
     // word address = 0xE000;
